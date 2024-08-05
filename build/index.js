c2cabad7490a6318af2f85bad6fc27bf92bb4615
Alabiabdullahi <abdullahialabi234@gmail.com>
2024-08-05 19:53:46 +0100

    creator controller code add up

--- build/index.js
+++ build/index.js
@@ -12,12 +12,12 @@ require("./authentication/creatorAuth");
 const eventRoute_1 = __importDefault(require("./routes/eventRoute"));
 const creatorRoute_1 = __importDefault(require("./routes/creatorRoute"));
 const path_1 = __importDefault(require("path"));
-const corsMiddleware_1 = __importDefault(require("./middleware/corsMiddleware"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const paymentRoute_1 = __importDefault(require("./routes/paymentRoute"));
 const logger_1 = __importDefault(require("./logging/logger"));
 const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 (0, dbconnection_1.default)();
 const app = (0, express_1.default)();
@@ -29,7 +29,7 @@ const limiter = (0, express_rate_limit_1.default)({
 });
 app.use(limiter);
 app.use((0, helmet_1.default)());
-app.use(corsMiddleware_1.default);
+app.use((0, cors_1.default)());
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
@@ -41,6 +41,11 @@ app.use("/user", userRoute_1.default);
 app.use("/creator", creatorRoute_1.default);
 app.use("/event", eventRoute_1.default);
 app.use("/payment", paymentRoute_1.default);
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to Eventful API",
+    });
+});
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     logger_1.default.info(`Listening on port ${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import PaymentRoute from "./routes/paymentRoute";
 import logger from "./logging/logger";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
+import cors from "cors";
 
 dotenv.config();
 
@@ -31,7 +32,7 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(helmet());
 
-app.use(corsMiddleware);
+app.use(cors());
 app.use(cookieParser());
 
 app.use(express.json());
