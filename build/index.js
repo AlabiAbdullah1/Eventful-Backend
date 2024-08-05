d9f4363e2fed2e176243ce411ae97df6cdbce86c
Alabiabdullahi <abdullahialabi234@gmail.com>
2024-08-05 20:43:57 +0100

    General code cleanup

--- build/index.js
+++ build/index.js
@@ -23,7 +23,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const limiter = (0, express_rate_limit_1.default)({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 300, // Limit each IP to 300 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
